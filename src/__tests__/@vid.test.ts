1f27894a317c8dcaa81550a8651a36d7f7791e2c
andremacola <andremacola@gmail.com>
2023-12-07 12:50:54 -0300

    fix: vid download

--- src/__tests__/@vid.test.ts
+++ src/__tests__/@vid.test.ts
@@ -1,11 +1,14 @@
-import { getVideoFileUrl } from '../whatsapp/commands/@vid'
+import { getVideoFile } from '../whatsapp/commands/@vid'
+// import { MessageMedia } from 'whatsapp-web.js'
 
 async function vid() {
   const url = 'https://www.youtube.com/watch?v=kuiZjitq2hU'
 
   try {
-    const videoUrl = await getVideoFileUrl(url)
-    console.log('video', videoUrl)
+    const video = await getVideoFile(url)
+    // const media = MessageMedia.fromFilePath(video._filename)
+
+    console.log('video', video)
   } catch (error) {
     console.log('getVideoFile =>', error)
   }
